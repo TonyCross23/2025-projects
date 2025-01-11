a0f01792faa0777f623e9362341bb6ab889174b0
TonyCross23 <tonychamp40@gmail.com>
2025-01-11 20:55:33 +0630

    error-fix

--- DevOps/index.js
+++ DevOps/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP } = require("./configs/config.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect("mongodb://root:mypassword@mongo:27017/?authSource=admin")
-  .then(() => console.log("Succefully connected db"))
-  .catch((e) => console.log(e));
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:27017/?authSource=admin`
+  )
+  .then(() => console.log("Successfully connected to the database"))
+  .catch((e) => console.error("Database connection error:", e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello World , i am Tony, Hehe<h2>");
