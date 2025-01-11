507b7bad4152debdbd4563008da38f72345c0233
TonyCross23 <tonychamp40@gmail.com>
2025-01-11 20:19:00 +0630

    mongodb-connect

--- DevOps/index.js
+++ DevOps/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect("mongodb://root:mypassword@mongo:27017/?authSource=admin")
+  .then(() => console.log("Succefully connected db"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello World , i am Tony, Hehe<h2>");
