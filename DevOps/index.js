cbe46d59651391d3b040bb396b8acd0b6d5def45
TonyCross23 <tonychamp40@gmail.com>
2025-01-12 19:00:30 +0630

    problem-sloved

--- DevOps/index.js
+++ DevOps/index.js
@@ -5,12 +5,19 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_IP } = require("./configs/config.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose
-  .connect(
-    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:27017/?authSource=admin`
-  )
-  .then(() => console.log("Successfully connected to the database"))
-  .catch((e) => console.error("Database connection error:", e));
+const connectWithRetry = () => {
+  mongoose
+    .connect(
+      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:27017/?authSource=admin`
+    )
+    .then(() => console.log("Successfully connected to the database"))
+    .catch((e) => {
+      console.error("Database connection error:", e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello World , i am Tony, Hehe<h2>");
