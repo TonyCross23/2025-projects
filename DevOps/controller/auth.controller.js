60c34acae1e420c52b3a3f29a7ae13ad620a9c85
TonyCross23 <tonychamp40@gmail.com>
2025-01-13 21:42:27 +0630

    auth

---
+++ DevOps/controller/auth.controller.js
@@ -0,0 +1,51 @@
+const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
+
+exports.signUp = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashpassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      username,
+      password: hashpassword,
+    });
+    res.status(201).json({
+      status: "Success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+    const isCorrect = await bcrypt.compare(password, user.password);
+    if (isCorrect) {
+      res.status(200).json({
+        status: "Success",
+      });
+    } else {
+      res.status(400).json({
+        status: "fail",
+        message: "username or password incorrect",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
--- DevOps/index.js
+++ DevOps/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP } = require("./configs/config.js");
 const postRouter = require("./routes/post.router.js");
+const userRouter = require("./routes/user.router.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +23,7 @@ connectWithRetry();
 
 app.use(express.json());
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello World , i am Tony, Hehe<h2>");
